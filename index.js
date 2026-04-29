2ab8c096fc0dcf235029ddb8897ae69624e8dbf6
cgpvicto-2436889 <filoupoupo@gmail.com>
2026-04-29 14:54:18 -0400

    V14

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import livresRoutes from "./routes/livres.routes.js";
-import pretsRoutes from "./routes/prets.routes.js";
-import utilisateursRoutes from "./routes/utilisateurs.routes.js";
+import livresRoutes from "./src/routes/livres.routes.js";
+import pretsRoutes from "./src/routes/prets.routes.js";
+import utilisateursRoutes from "./src/routes/utilisateurs.routes.js";
 /* Viens du site du cours */
 import authentification from './middlewares/authentification.middleware.js';
 
