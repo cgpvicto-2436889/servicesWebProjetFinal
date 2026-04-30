c5f07c147d6bca8f610967eddd0972f5dce9a5f7
cgpvicto-2436889 <filoupoupo@gmail.com>
2026-04-30 10:23:35 -0400

    V36

--- src/config/db_pg.js
+++ src/config/db_pg.js
@@ -4,6 +4,13 @@ import pg from 'pg'
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log("--- TEST DE CONNEXION ---");
+console.log("DB_USER:", process.env.PG_USER);
+console.log("DB_HOST:", process.env.PG_HOST);
+console.log("DB_PASS:", process.env.PG_PASSWORD ? "REÇU" : "VIDE");
+console.log("DB_SSL:", process.env.PG_SSL);
+console.log("-------------------------");
+
 let params = { 
   database: process.env.PG_DATABASE,  
   host: process.env.PG_HOST,
