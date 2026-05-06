1f1b2bebea2775ddaa28f4b48a178def59fd5918
cgpvicto-2436889 <filoupoupo@gmail.com>
2026-05-06 14:24:13 -0400

    V57

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import pool from "./src/config/db_pg.js";
 import cors from "cors";
-import express from 'express';
+import express from "express";
 import livresRoutes from "./src/routes/livres.routes.js";
 import pretsRoutes from "./src/routes/prets.routes.js";
 import utilisateursRoutes from "./src/routes/utilisateurs.routes.js";
-/* Viens du site du cours */
-import authentification from './src/middlewares/authentification.middleware.js';
+import authentification from "./src/middlewares/authentification.middleware.js";
+
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+
+const swaggerDocument = JSON.parse(fs.readFileSync("./src/config/documentation.json", "utf8"));
+
+const swaggerOptions = {
+    customCss: ".swagger-ui .topbar { display: none }",
+    customSiteTitle: "API Bibliothèque"
+};
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/livres', authentification, livresRoutes);
-app.use('/api/prets', authentification, pretsRoutes);
-app.use('/api/utilisateurs', utilisateursRoutes);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
+
+app.use("/api/livres", authentification, livresRoutes);
+app.use("/api/prets", authentification, pretsRoutes);
+app.use("/api/utilisateurs", utilisateursRoutes);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ interface/js/gestion-utilisateurs.js
@@ -0,0 +1,106 @@
+const courrielRecuperer = document.getElementById("courriel-recuperer").value;
+const passwordRecuperer = document.getElementById("password-recuperer").value;
+const bouttonRecuperer = document.getElementById("boutton-recuperer");
+
+const nomAjout = document.getElementById("nom").value;
+const courrielAjout = document.getElementById("courriel-ajout").value;
+const passwordAjout = document.getElementById("password-ajout").value;
+const bouttonAjout = document.getElementById("boutton-ajout");
+
+const checkboxChangerCle = document.getElementById("modifier-cle");
+
+bouttonAjout.addEventListener("click", ajouterUtilisateurs());
+
+
+function ajouterUtilisateurs() {
+    if (courrielAjout == null) {
+        return res.status(400).json({ message: "Le courriel est obligatoire" });
+    }
+    if (passwordAjout == null) {
+        return res.status(400).json({ message: "Le mot de passe est obligatoire" });
+    }
+    
+    const reponse = await fetch("https://serviceswebprojetfinal.onrender.com/api/utilisateurs/ajouterUtilisateur", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            nom: nomAjout,
+            courriel: courrielAjout,
+            password: passwordAjout
+        })
+    });
+
+    if (reponse.ok) {
+        const resultat = await reponse.json();
+        console.log("Resultat :", resultat);
+        
+        if (reponse.ok) {
+            console.log("Utilisateur ajouté :", resultat);
+            alert("Compte créé avec succès. Ta clé API est : " + resultat.cle_api);
+        }
+    } else {
+        console.log("Erreur :", resultat);
+        alert(resultat.message);
+    }
+}
+
+function ajouterUtilisateurs() {
+    if (courrielRecuperer == null) {
+        return res.status(400).json({ message: "Le courriel est obligatoire" });
+    }
+    if (passwordRecuperer == null) {
+        return res.status(400).json({ message: "Le mot de passe est obligatoire" });
+    }
+    
+    if (checkboxChangerCle.checked) {
+        const reponse = await fetch("https://serviceswebprojetfinal.onrender.com/api/utilisateurs/recupererCleApi", {
+            method: 'patch',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                "courriel": courrielRecuperer,
+                "password": passwordRecuperer
+            })
+        });
+
+        if (reponse.ok) {
+            const resultat = await reponse.json();
+            console.log("Resultat :", resultat);
+            
+            if (reponse.ok) {
+                console.log("Utilisateur ajouté :", resultat);
+                alert("clé récuperer avec succes. Ta clé API est : " + resultat.cle_api);
+            }
+        } else {
+            console.log("Erreur :", "probleme");
+            alert(resultat.message);
+        }
+    } else {
+        const reponse = await fetch("https://serviceswebprojetfinal.onrender.com/api/utilisateurs/recupererCleApi", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                "courriel": courrielRecuperer,
+                "password": passwordRecuperer
+            })
+        });
+
+        if (reponse.ok) {
+            const resultat = await reponse.json();
+            console.log("Resultat :", resultat);
+            
+            if (reponse.ok) {
+                console.log("Utilisateur ajouté :", resultat);
+                alert("clé récuperer avec succes. Ta clé API est : " + resultat.cle_api);
+            }
+        } else {
+            console.log("Erreur :", "probleme");
+            alert(resultat.message);
+        }
+    }
+}
